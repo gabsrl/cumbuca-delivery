a3a5d793d6bcd2cf77c9b3f521ed065bb77ee3b3
Gabriel Lima <grl@icomp.ufam.edu.br>
2024-02-21 22:15:44 -0400

    feat: adding Dish type and some mocking abour dishes

--- cumbuca-frontend/src/types/dish.type.ts
+++ cumbuca-frontend/src/types/dish.type.ts
@@ -1,16 +1,23 @@
 export type Dish = {
-    id: number;
-    name: string;
-    description: string;
-    price: number
-    category: string;
-    enable: boolean;
-    image: File;
-    isVegan?: boolean;
-    servings?: number;
-}
+  id: number;
+  name: string;
+  description: string;
+  price: number;
+  category: string;
+  enable: boolean;
+  image: File | string;
+  isVegan?: boolean;
+  servings?: number;
+  weight: number; // Peso em gramas
+  nutrition?: {
+    totalCalories: number; // Calorias totais
+    fatCalories: number; // Calorias de gordura
+    totalFat: number; // Gordura total em gramas
+    saturedFat: number; // Gordura saturada em gramas
+    transFat: number; // Gordura trans em gramas
+    sugar: number; // Açúcar em gramas
+  };
+  allergens: string; // Alergênicos presentes
+};
 
-
-export type CreateDishDto = Omit<Dish, "id"> & {
-
-}
+export type CreateDishDto = Omit<Dish, 'id'>;
