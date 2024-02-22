c1552ac823fa5850b4f6a26607a10fb83508493a
Gabriel Rocha <gabriel.rocha@grupoicts.com.br>
2024-02-22 11:06:05 -0400

    feat: added new utilities to the miscellaneous

--- cumbuca-frontend/src/utils/miscellaneous.ts
+++ cumbuca-frontend/src/utils/miscellaneous.ts
@@ -1,5 +1,26 @@
-import { Dish } from '../types/Dish.type';
+import { Dish } from '../types/dish.type';
+
+
+const BRL_CURRENCY_FORMATTER = Intl.NumberFormat('pt-BR', {
+  currency: 'BRL', style: 'currency', minimumFractionDigits: 2,
+  maximumFractionDigits: 2
+});
 
 export function isPromo(dish: Dish) {
   return dish.price <= 20;
+}
+
+export function isVegan(dish: Dish) {
+  return dish.isVegan;
+}
+
+
+export function BRLCurrency(value: number) {
+  return BRL_CURRENCY_FORMATTER.format(value);
+}
+
+export function WeightFormmater(weight: number) {
+  if (weight >= 1000) return { value: `${Math.round(weight / 1000)}`, unit: 'kg' }
+
+  return { value: `${Math.round(weight)}`, unit: 'g' }
 }
