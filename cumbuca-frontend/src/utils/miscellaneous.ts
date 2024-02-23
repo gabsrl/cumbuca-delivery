6a9ffb57177e87e3e60a770215c6ad5d415c500f
Gabriel Lima <grl@icomp.ufam.edu.br>
2024-02-22 20:55:25 -0400

    feat: updated utilities

--- cumbuca-frontend/src/utils/miscellaneous.ts
+++ cumbuca-frontend/src/utils/miscellaneous.ts
@@ -1,9 +1,10 @@
 import { Dish } from '../types/dish.type';
 
-
 const BRL_CURRENCY_FORMATTER = Intl.NumberFormat('pt-BR', {
-  currency: 'BRL', style: 'currency', minimumFractionDigits: 2,
-  maximumFractionDigits: 2
+  currency: 'BRL',
+  style: 'currency',
+  minimumFractionDigits: 2,
+  maximumFractionDigits: 2,
 });
 
 export function isPromo(dish: Dish) {
@@ -14,16 +15,21 @@ export function isVegan(dish: Dish) {
   return dish.isVegan;
 }
 
-
 export function BRLCurrency(value: number) {
   return BRL_CURRENCY_FORMATTER.format(value);
 }
 
 export const WeightFormmater = (weight: number) => {
+  if (weight >= 1000)
+    return {
+      value: `${Math.round(weight / 1000)}`,
+      unit: 'kg',
+      toString: () => `${Math.round(weight / 1000)}kg`,
+    };
 
-
-  if (weight >= 1000) return { value: `${Math.round(weight / 1000)}`, unit: 'kg', toString: () => `${Math.round(weight / 1000)}` }
-
-  return { value: `${Math.round(weight)}`, unit: 'g', toString: () => `${Math.round(weight)}g` }
-
-}
+  return {
+    value: `${Math.round(weight)}`,
+    unit: 'g',
+    toString: () => `${Math.round(weight)}g`,
+  };
+};
